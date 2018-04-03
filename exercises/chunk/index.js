325dcd513121fb9e6e5e4482773c98f87b9fd726
PJC-1 <jumpzine@gmail.com>
2018-04-02 22:48:56 -0700

    added chunked solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunked = [];
+  for(let item of array) {
+    const last = chunked[chunked.length -1];
+    if(!last || last.length === size) {
+      chunked.push([item])
+    } else {
+      last.push(item);
+    }
+  }
+  return chunked;
+}
 
 module.exports = chunk;
