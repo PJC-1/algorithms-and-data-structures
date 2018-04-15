13013c7ba3391b59bdff4b175e573d97d1291c3d
PJC-1 <jumpzine@gmail.com>
2018-04-14 20:57:06 -0700

    added iterative solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const columnTotal = 2 * n - 1;
+  const midpoint = Math.floor(columnTotal/2);
+
+  for(let row = 0; row < n; row++) {
+    let level = '';
+    for(let column = 0; column < columnTotal; column++) {
+      if(column >= midpoint - row && column <= midpoint + row) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
