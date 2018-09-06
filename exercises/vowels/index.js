32792202580e2cc2fd3cfff317c3579c8f9076bf
PJC-1 <jumpzine@gmail.com>
2018-09-05 22:12:43 -0700

    vowels solution 2 with regex

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,17 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+
 function vowels(str) {
-  let counter = 0;
-  const checker = ['a', 'e', 'i', 'o', 'u'];
-
-  for (let char of str.toLowerCase()) {
-    if (checker.includes(char)) {
-      counter++;
-    }
-  }
-
-  return counter;
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
+
+// Solution 1
+
+// function vowels(str) {
+//   let counter = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       counter++;
+//     }
+//   }
+//
+//   return counter;
+// }
 
 module.exports = vowels;
