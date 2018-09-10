cab005a428a710a93e96b8439b8278278bde6a78
PJC-1 <jumpzine@gmail.com>
2018-09-09 22:30:27 -0700

    fibonacci iterative solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,17 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[result.length - 1];
+    const b = result[result.length - 2];
+
+    result.push(a + b);
+  }
+
+  return result[n];
+}
 
 module.exports = fib;
