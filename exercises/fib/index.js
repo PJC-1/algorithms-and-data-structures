c80f863298e70ef8e0459ccf84be449c8028b440
PJC-1 <jumpzine@gmail.com>
2018-09-10 21:10:43 -0700

    fibonacci recursive solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,16 +9,27 @@
 //   fib(4) === 3
 
 function fib(n) {
-  const result = [0, 1];
-
-  for (let i = 2; i <= n; i++) {
-    const a = result[result.length - 1];
-    const b = result[result.length - 2];
-
-    result.push(a + b);
+  if (n < 2) {
+    return n;
   }
 
-  return result[n];
+  return fib(n - 1) + fib(n - 2);
 }
+
+
+// Solution 1
+
+// function fib(n) {
+//   const result = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[result.length - 1];
+//     const b = result[result.length - 2];
+//
+//     result.push(a + b);
+//   }
+//
+//   return result[n];
+// }
 
 module.exports = fib;
