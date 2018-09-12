00e962484ad2a22bc766634863e93bf72895ac20
PJC-1 <jumpzine@gmail.com>
2018-09-11 21:58:44 -0700

    recursive with memoized solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,13 +8,39 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoize(slowFib);
+
+// Solution 2
+
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//
+//   return fib(n - 1) + fib(n - 2);
+// }
 
 
 // Solution 1
